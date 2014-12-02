cd0d683cd0c4776335e83cc75afa3a1b4520f4d4
litianzhao <litianzhao@tangpool.com>
2014-12-02 11:15:23 +0800

    添加了算力检查

---
+++ scripts/modify_pool/check_share.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eu
+
+ip="$1"
+threshold="$2"
+
+sshpass -p root \
+    ssh -T -o UserKnownHostsFile=/dev/null \
+        -o StrictHostKeyChecking=no \
+        -o ConnectTimeout=30 \
+    root@"$ip" "
+threshold=$threshold;
+share=`cgminer-api | sed -n 's| \+\[GHS av\] => \([[:digit:]]\+\).\+|\1|gp'`;
+if [[ \"\$share\" -lt \"\$threshold\" ]]; then
+    echo lower than expected \"\$threshold\", restarting... >&2 ;
+    /etc/init.d/cgminer restart ;
+fi
+"
